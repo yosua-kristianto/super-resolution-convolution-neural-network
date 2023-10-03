e0920d9fb30c4a5c49f935adb3347677142a8914
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-03 11:51:27 +0700

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Step by step to do Super Resolution Convolutional Neural Network but in F#
+
+This code based on https://github.com/yosua-kristianto/thesis-super-resolution-model
---
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/Program.fs
@@ -0,0 +1,21 @@
+﻿open System;
+open Tensorflow;
+open Tensorflow.NumPy;
+open Tensorflow.Keras.Engine;
+open Tensorflow.Keras.Layers;
+
+
+let CreateModel (height: int) (width: int) (depth: int): Tensorflow.Keras.Engine.IModel =
+    
+    //Tensorflow.Keras.Layers
+    let inputShape: Tensorflow.Shape = new Tensorflow.Shape([| height; width; depth |]);
+    let layerArguments: Tensorflow.Keras.ArgsDefinition.LayerArgs = new Tensorflow.Keras.ArgsDefinition.LayerArgs();
+    layerArguments.InputShape <- inputShape;
+
+    let inputLayer = Tensorflow.Keras.Engine
+    
+    let model: Tensorflow.Keras.Engine.IModel = Tensorflow.Keras.Engine.IModel()
+
+
+
+    model;
