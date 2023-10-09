8489f6c0f360bcd7c0f0c7a1f1cb76849c77315e
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-09 12:17:56 +0700

    Added some progress

--- Src/Main/Machine Learning Instance/Machine Learning Instance/Program.fs
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/Program.fs
@@ -3,19 +3,65 @@ open Tensorflow;
 open Tensorflow.NumPy;
 open Tensorflow.Keras.Engine;
 open Tensorflow.Keras.Layers;
+open Tensorflow.Keras.ArgsDefinition;
 
+open type Tensorflow.Binding;
 
-let CreateModel (height: int) (width: int) (depth: int): Tensorflow.Keras.Engine.IModel =
+let CreateModel (height: int) (width: int) (depth: int): IModel =
     
-    //Tensorflow.Keras.Layers
-    let inputShape: Tensorflow.Shape = new Tensorflow.Shape([| height; width; depth |]);
-    let layerArguments: Tensorflow.Keras.ArgsDefinition.LayerArgs = new Tensorflow.Keras.ArgsDefinition.LayerArgs();
-    layerArguments.InputShape <- inputShape;
-
-    let inputLayer = Tensorflow.Keras.Engine
+    let input = tf.keras.layers.Input(new Shape([| height; width; depth |]));
     
-    let model: Tensorflow.Keras.Engine.IModel = Tensorflow.Keras.Engine.IModel()
+    // Setting Hidden Layer 1
+    //let mutable x = tf.keras.layers.Conv2D(64, kernel_size=new Shape([| 9; 9 |]), kernel_initializer="he_normal");
+
+    let data = x input;
+    
+    null;
 
 
+(*
 
-    model;
+I think the problem is not with the open type statement, but with the way you are calling the Conv2D layer. In TensorFlow.NET, you need to use the functional API of the library, which allows you to create layers as callable objects and pass tensors to them. For example, if you want to create a Conv2D layer with 64 filters of size 9x9 and He normal initialization, and apply it to the input tensor, you can write:
+
+```fsharp
+open Tensorflow
+open Tensorflow.Keras
+
+// create a Conv2D layer with 64 filters of size 9x9 and He normal initialization
+let conv2d = Layers.Conv2D(64, (9, 9), kernel_initializer="he_normal")
+
+// apply the Conv2D layer to the input tensor
+let output = conv2d input
+```
+
+This will produce an output tensor with shape (batch_size, new_height, new_width, 64), where new_height and new_width depend on the padding and strides arguments of the Conv2D layer. By default, the padding is "valid", which means no padding is applied, and the strides are (1, 1), which means the convolution window moves by one pixel along each dimension¹.
+
+So, in your code, you should change this line:
+
+```fsharp
+let mutable x = tf.keras.layers.Conv2D(64, kernel_size=new Shape([| 9; 9 |]), kernel_initializer="he_normal");
+```
+
+to this:
+
+```fsharp
+let conv2d = Layers.Conv2D(64, (9, 9), kernel_initializer="he_normal");
+```
+
+And then call it like this:
+
+```fsharp
+let data = conv2d input;
+```
+
+This should fix the error and allow you to create a Conv2D layer in TensorFlow.NET. For more information about the Conv2D layer and other layers in TensorFlow.NET, you can check out the results from my search¹.
+
+Source: Conversation with Bing, 10/9/2023
+(1) Error fitting images into Conv2D layer in tensorflow Keras. https://stackoverflow.com/questions/53957844/error-fitting-images-into-conv2d-layer-in-tensorflow-keras.
+(2) tf.keras.layers.Conv2D | TensorFlow v2.14.0. https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2D.
+(3) Tensorflow Keras Conv2D error with 2D numpy array input. https://stackoverflow.com/questions/60648191/tensorflow-keras-conv2d-error-with-2d-numpy-array-input.
+(4) UNIMPLEMENTED: DNN library is not found. #10590 - GitHub. https://github.com/tensorflow/models/issues/10590.
+(5) TFLite Converter: Conv2D error when converting - TensorFlow Forum. https://discuss.tensorflow.org/t/tflite-converter-conv2d-error-when-converting/10830.
+
+
+*)
