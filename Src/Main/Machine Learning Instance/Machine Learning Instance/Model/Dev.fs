f5874c4aac53c8e4db490484f44f0951fcb45d66
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-09 18:21:01 +0700

    Added currently inprogress model

---
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/Model/Dev.fs
@@ -0,0 +1,83 @@
+﻿module Model_V_Dev
+
+open Tensorflow;
+open Tensorflow.Keras.Engine;
+
+open type Tensorflow.Binding;
+
+// The working chain from 20231009
+let CreateModel (height: int) (width: int) (depth: int): IModel =
+
+    // Setting Input Layer
+    let input = tf.keras.layers.Input(new Shape([| height; width; depth |]));
+
+    // Setting Hidden Layer 1
+    let x_l1 = tf.keras.layers.Conv2D(64, kernel_size=new Shape([| 9; 9 |]), kernel_initializer="he_normal", activation="relu");
+    let x_l2 = tf.keras.layers.Conv2D(32, kernel_size=new Shape([| 1; 1 |]), kernel_initializer="he_normal", activation="relu");
+
+    // Output Layer
+    let output = tf.keras.layers.Conv2D(depth, kernel_size=new Shape([| 5; 5 |]), kernel_initializer="glorot_uniform");
+
+    // Chaining Tensors
+    let mutable x: Tensors = x_l1.Apply(input); // Apply input to x_l1
+    x <- x_l2.Apply(x); // Apply x to x_l2
+    x <- output.Apply(x);
+
+    tf.keras.Model(input, x);
+    
+
+(*
+REFERENCES:
+
+I'm glad you found a working solution for your code. It seems that you are using the TensorFlow.NET framework directly, instead of the Keras.NET library, which is a high-level wrapper for TensorFlow.NET. I apologize for the confusion and the inconvenience that I might have caused you with my previous suggestions.
+
+Your code looks good and follows the functional API of TensorFlow.NET, which allows you to create layers as callable objects and pass tensors to them. You are also using the "Apply" method of the layers, which is the correct syntax for applying them to the input tensor. You are also chaining the tensors together using the "<-" operator, which assigns the result of one expression to a mutable variable.
+
+However, I noticed that you are using a "he_normal" initializer for all your Conv2D layers, which might not be optimal for your output layer. The "he_normal" initializer is suitable for networks with ReLU activation functions, as it helps to avoid vanishing or exploding gradients. However, your output layer does not have an activation function, which means it will output linear values. This might cause problems if your output values are too large or too small compared to your input values.
+
+A possible solution is to use a different initializer for your output layer, such as "glorot_uniform" or "glorot_normal", which are also known as Xavier initializers. These initializers draw samples from a uniform or normal distribution with mean 0 and variance 2 / (fan_in + fan_out), where fan_in and fan_out are the number of input and output units in the weight tensor. These initializers are suitable for networks with linear or sigmoid activation functions, as they help to preserve the variance of the inputs and outputs.
+
+To use a different initializer for your output layer, you can change this line:
+
+```fsharp
+// Output Layer
+let output = tf.keras.layers.Conv2D(depth, kernel_size=new Shape([| 5; 5 |]), kernel_initializer="he_normal");
+```
+
+to this:
+
+```fsharp
+// Output Layer
+let output = tf.keras.layers.Conv2D(depth, kernel_size=new Shape([| 5; 5 |]), kernel_initializer="glorot_uniform");
+```
+
+This should improve the performance of your model and avoid potential issues with your output values. For more information about weight initialization methods and their effects on neural networks, you can check out [this link].
+
+
+
+
+Xavier initialization is a way of setting the initial values of the weights in a neural network. It helps to avoid problems with the training process, such as vanishing or exploding gradients, which can make the network learn very slowly or not at all.
+
+The idea behind Xavier initialization is to make the weights have a suitable scale for the input and output of each layer. If the weights are too small, the signal from the input will get weaker as it passes through the network, and the gradients will become very small. This is called vanishing gradients, and it makes the network hard to update and learn. If the weights are too large, the signal from the input will get stronger as it passes through the network, and the gradients will become very large. This is called exploding gradients, and it makes the network unstable and prone to errors.
+
+Xavier initialization tries to balance these two problems by making the weights have a variance that depends on the number of input and output units in each layer. The variance is a measure of how much the weights vary around their mean value. A higher variance means more variation, and a lower variance means less variation. Xavier initialization uses a formula to calculate the variance based on the input and output units, and then draws random values from a distribution with that variance. This way, the weights have a scale that is appropriate for each layer, and the signal and gradients can flow smoothly through the network.
+
+You can use Xavier initialization in TensorFlow.NET by using the tf.initializers.GlorotUniform() or tf.initializers.GlorotNormal() functions, which are also known as Glorot uniform and Glorot normal distributions. These are two types of distributions that follow the Xavier initialization formula. The difference is that Glorot uniform draws values from a uniform distribution, which has equal probability for any value in a range, while Glorot normal draws values from a normal distribution, which has more probability for values near the mean and less probability for values far from the mean. You can pass these functions as arguments to the kernel_initializer parameter of your Conv2D layers¹². For example:
+
+```fsharp
+open Tensorflow
+open Tensorflow.Keras
+
+// create a Conv2D layer with 64 filters of size 9x9 and Xavier initialization using Glorot uniform
+let conv2d = Layers.Conv2D(64, (9, 9), kernel_initializer=tf.initializers.GlorotUniform())
+
+// apply the Conv2D layer to the input tensor
+let output = conv2d input
+```
+
+I hope this helps you understand what Xavier initialization is and how to use it in TensorFlow.NET. For more information about Xavier initialization and other weight initialization methods, you can check out [this link].
+
+Source: Conversation with Bing, 10/9/2023
+(1) What Is Xavier Initialization? | 365 Data Science. https://365datascience.com/tutorials/machine-learning-tutorials/what-is-xavier-initialization/.
+(2) How to do Xavier initialization on TensorFlow - Stack Overflow. https://stackoverflow.com/questions/33640581/how-to-do-xavier-initialization-on-tensorflow.
+*)
