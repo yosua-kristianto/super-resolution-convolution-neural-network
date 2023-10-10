3464f792083c82db474ea5e00d712374c376d74a
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-10 11:38:26 +0700

    Added Log Module

---
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/Facade/Logger.fs
@@ -0,0 +1,12 @@
+﻿namespace Facade
+
+open System.Configuration;
+
+module Log =
+
+    (*
+        WriteLog
+    *)
+    let WriteLog (logMessage: string) (logCategory: string) = 
+        printfn "%s" logMessage
+
