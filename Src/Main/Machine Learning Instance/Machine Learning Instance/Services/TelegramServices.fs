19ba82b3a993bb09aa70bef554b64443fb237e7d
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-10 17:08:15 +0700

    Added commentaries

--- Src/Main/Machine Learning Instance/Machine Learning Instance/Services/TelegramServices.fs
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/Services/TelegramServices.fs
@@ -3,12 +3,18 @@
 open System.Net.Http;
 
 module TelegramServices = 
+
+    (*
+      SendMessage
+
+      This function will send message through Telegram chat to destinated channel. 
+    *)
     let SendMessage (message: string) =
         let httpClient = new HttpClient();
 
         // Request generation
         let uri = "https://api.telegram.org/bot:id/sendMessage?chat_id=chat:id&text=" + message;
 
-        // Just send the message
+        // @async Just send the message
         httpClient.PostAsync(uri, null);
 
