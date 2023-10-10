950a016036c9afb901b7cbbc775aedd96b0566d3
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-10 11:38:37 +0700

    Added Telegram Services

---
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/Services/TelegramServices.fs
@@ -0,0 +1,14 @@
+﻿namespace Services
+
+open System.Net.Http;
+
+module TelegramServices = 
+    let SendMessage (message: string) =
+        let httpClient = new HttpClient();
+
+        // Request generation
+        let uri = "https://api.telegram.org/bot:id/sendMessage?chat_id=chat:id&text=" + message;
+
+        // Just send the message
+        httpClient.PostAsync(uri, null);
+
