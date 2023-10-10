a07a7b83dd1aa967fc7bec378bfc2984bf540990
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-10 11:39:24 +0700

    Added LoadImage function

--- Src/Main/Machine Learning Instance/Machine Learning Instance/TrainingPreprocessor/Main.fs
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/TrainingPreprocessor/Main.fs
@@ -1,8 +1,13 @@
 ﻿namespace Main
 
+open System;
 open System.IO;
 
-module Main =
-  let LoadImage =
-    let imageDirectory: string = "";
+module LoadImage =
+    let currentTime: DateTime = DateTime.Now;
 
+    let imageDirectory: string = "E:\\New folder\\TrainTest";
+    let currentSession: string = currentTime.ToString("yyyyMMdd HHmm");
+
+    for file in Directory.GetFiles(imageDirectory) do
+        printfn "%s" file
