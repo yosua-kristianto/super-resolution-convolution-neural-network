2fac65c8d3dd02cb857ed9cef31273218ec7818e
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-10 17:12:45 +0700

    Obj

--- Src/Main/Machine Learning Instance/Machine Learning Instance/TrainingPreprocessor/Main.fs
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/TrainingPreprocessor/Main.fs
@@ -1,13 +1,38 @@
-﻿namespace Main
+﻿namespace TrainingPreprocessor
 
 open System;
 open System.IO;
+open TrainingPreprocessor.PythonLongerHand;
 
 module LoadImage =
-    let currentTime: DateTime = DateTime.Now;
+    let Main = 
+        let currentTime: DateTime = DateTime.Now;
 
-    let imageDirectory: string = "E:\\New folder\\TrainTest";
-    let currentSession: string = currentTime.ToString("yyyyMMdd HHmm");
+        let imageDirectory: string = "D:\\storage\\training-dataset";
+        let currentSession: string = currentTime.ToString("yyyyMMdd HHmm");
 
-    for file in Directory.GetFiles(imageDirectory) do
-        printfn "%s" file
+        // This flag used for making a sequence for the processed image file 
+        let mutable i: int = 0;
+
+        for file in Directory.GetFiles(imageDirectory) do
+            printfn "%s" file;
+
+            // Limit process for 1 image only for debugging
+            if i <> 0 then
+                Console.WriteLine("Processing 1 data complete");
+                ()
+
+            // Prepare folder for the destinated file
+            let folderName: string = currentSession + "_" + string(i);
+            //Console.WriteLine(folderName);
+
+            printfn "Proposed folder name is: %s" folderName;
+
+            let imageArray = ImageToArray file;
+
+            i <- i+1;
+
+            
+            // Get file full path
+            //let fileFullPath: string = imageDirectory + file;
+
