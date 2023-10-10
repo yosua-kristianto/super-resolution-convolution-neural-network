bb356b91215c5213ec542d531930a27472821283
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-10 11:39:13 +0700

    Added constant

--- Src/Main/Machine Learning Instance/Machine Learning Instance/TrainingPreprocessor/Constant.fs
+++ Src/Main/Machine Learning Instance/Machine Learning Instance/TrainingPreprocessor/Constant.fs
@@ -10,18 +10,9 @@ Here we define some parameters:
 | `PAD` | Padding that need to be added to output patches |
 | `STRIDE` | Larger STRIDE will result in higher pixel skipping. Which will reduce more image quality. On the dzlab's github, `STRIDE` explained as *"the stride which is the number of pixels we'll slide both in the horizontal and vertical axes to extract patches"* |
 *)
-
-module SCALE =
-  let value: float32 = 2.0f;
-
-module INPUT_DIM =
-  let value: int = 33;
-
-module LABEL_SIZE =
-  let value: int = 21;
-
-module PAD =
-  let value: int = int(float32(INPUT_DIM.value - LABEL_SIZE.value) / 2.0f);
-
-module STRIDE =
-  let value: int = 14;
+module HyperParameters =
+    let SCALE: float32 = 2.0f;
+    let INPUT_DIM: int = 33;
+    let LABEL_SIZE: int = 21;
+    let PAD: int = int(float32(INPUT_DIM - LABEL_SIZE) / 2.0f);
+    let STRIDE: int = 14;
